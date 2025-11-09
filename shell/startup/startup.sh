7f0ab65be9b8f49f2178954f920eefffa9aeef68
Cuppixx <CuppiXD@gmx.de>
2025-11-09 13:20:11 +0100

    adjusted startup.sh as spotify_player currently isnt working due to Spotify-API changes

--- shell/startup/startup.sh
+++ shell/startup/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-ping -c1 1.1.1.1 && { syncthing & obsidian & }
-konsole --new-tab --workdir /mnt/data/ -e fish &
-konsole --new-tab --workdir /mnt/data/ -e fish -c "spotify_player"
+ping -c1 1.1.1.1 && { syncthing & obsidian & spotify & konsole --new-tab --workdir /home/cuppixx/Music/ -e fish & }
+konsole --new-tab --workdir /mnt/data/ -e fish
